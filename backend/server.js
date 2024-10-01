b906e8690c4415a56acc3586fb16e00fe73b67ff
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-01 22:29:45 +0300

    trying to deploy the created part from the app

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,26 @@ import express from "express";
 import mongoose from "mongoose";
 
 import dotenv from "dotenv";
-
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ||  3001;
 
+//for deployment
+const __dirname = path.resolve();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Testing")
+})
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res)=> {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
 app.listen(PORT, () => {
