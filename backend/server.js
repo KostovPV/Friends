12d7f0a0ab6e2ec36ff16e094ea221dda4291d98
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-01 22:48:46 +0300

    Etited port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ||  3001;
+const PORT = process.env.PORT ||  4000;
 
 //for deployment
 const __dirname = path.resolve();
