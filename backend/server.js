3f35ef502891b5d7bb4315a34532947c559db2a8
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-02 13:00:53 +0300

    Etitedserver.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
-
 import dotenv from "dotenv";
 import path from 'path';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.VITE_PORT ||  5173;
+const PORT = process.env.VITE_PORT || 5173;
 
-//for deployment
+// Serve static files from the frontend dist folder
 const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Testing")
-})
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res)=> {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
+// If no static files are found, respond with the built index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
